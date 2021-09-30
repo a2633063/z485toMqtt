7b67b591c732953ed5aeac2fddd24b6ceee809f8
a2633063 <76966589@qq.com>
2021-09-30 17:15:56 +0800

    add: zlib库增加json/mqtt/ add: 增加部分测试函数

--- app/user/user_config.h
+++ app/user/user_config.h
@@ -5,12 +5,19 @@
 
 #include "os_type.h"
 
-#define BCDtoDEC(x) ( ((x)>>4)*10+(x)%0x10  )           //BCD��ת��Ϊʮ���Ʊ�ʾ��ʽ
-#define DECtoBCD(x) ( (((x)/10)<<4)+(x)%10  )           //ʮ����ת��ΪBCD���ʾ��ʽ
+#define BCDtoDEC(x) ( ((x)>>4)*10+(x)%0x10  )           //BCD码转换为十进制表示方式
+#define DECtoBCD(x) ( (((x)/10)<<4)+(x)%10  )           //十进制转换为BCD码表示方式
 
-#define ZLIB_WIFI_CALLBACK_REPEAT  (1)  //wifi���ӹ����� �Ƿ��ظ����ûص�����
-#define ZLIB_WIFI_MDSN_ENABLI  (1)      //�Ƿ�����mdns,Ĭ�ϲ�����mdns
+#define ZLIB_WIFI_CALLBACK_REPEAT  (0)  //wifi连接过程中 是否重复调用回调函数
+#define ZLIB_WIFI_MDSN_ENABLI  (1)      //是否启用mdns,默认不启用mdns
 
+#define ZLIB_UDP_REPLY_PORT  (10181)  //udp回复默认端口号
+#define ZLIB_WIFI_CONNECTED_STATE_LED  (1)  //wifi连接后 wifi状态指示灯io口电平
+//#define ZLIB_WIFI_STATE_LED_IO_MUX     PERIPHS_IO_MUX_GPIO2_U
+//#define ZLIB_WIFI_STATE_LED_IO_NUM     2
+//#define ZLIB_WIFI_STATE_LED_IO_FUNC    FUNC_GPIO2
+
+#define ZLIB_WEB_WIFI_ONLY  (1)  //若使用了自定义webserver 此应该设置为0 若仅用web配网 此置1 且调用zlib_web_wifi_init即可
 #define VERSION "v0.0.2"
 
 #define TYPE 99
@@ -21,15 +28,15 @@
 
 #define USER_CONFIG_VERSION 1
 
-#define SETTING_MQTT_STRING_LENGTH_MAX  64      //���� 4 �ֽڶ��롣
-#define NAME_LENGTH 32		//���������ַ�����󳤶�
+#define SETTING_MQTT_STRING_LENGTH_MAX  64      //必须 4 字节对齐。
+#define NAME_LENGTH 32		//插座名称字符串最大长度
 
-//����ֵ
-#define ALERT_OFF		0xff	//�ز���
-#define ALERT_NO_BEEP	0x02	//�������ط�����
-#define ALERT_WITH_BEEP	0x01	//��������������
+//布防值
+#define ALERT_OFF		0xff	//关布防
+#define ALERT_NO_BEEP	0x02	//开布防关蜂鸣器
+#define ALERT_WITH_BEEP	0x01	//开布防开蜂鸣器
 
-//�û���������ṹ��
+//用户保存参数结构体
 typedef struct
 {
     char version;
@@ -42,18 +49,18 @@ typedef struct
 } user_config_t;
 
 /*
- //����ͨ�����ݽṹ��
+ //串口通信数据结构体
  typedef struct {
- uint8_t flag;	//�Ƿ��Ѿ�����
+ uint8_t flag;	//是否已经更新
  uint8_t cmd;	//cmd
  uint16_t msgID1;	//msgID
- uint8_t reed;	//�ɻɹ�״̬
+ uint8_t reed;	//干簧管状态
  struct struct_time now;
  uint8_t msgID2;	//msgID
- uint8_t battery;	//����
- uint8_t alert;	//��������
- struct struct_time alert_time_start;	//����ʱ��
- struct struct_time alert_time_end;	//����ʱ��
+ uint8_t battery;	//电量
+ uint8_t alert;	//布防开关
+ struct struct_time alert_time_start;	//布防时间
+ struct struct_time alert_time_end;	//布防时间
  } user_sensor_t;*/
 extern user_config_t user_config;
 
--- app/user/user_main.c
+++ app/user/user_main.c
@@ -2,37 +2,90 @@
 #include "uart.h"
 #include "osapi.h"
 #include "user_interface.h"
-
 #include "user_config.h"
 #include "zlib.h"
+void ICACHE_FLASH_ATTR _ota_result_cb(state_ota_result_state_t b)
+{
+    os_printf("_ota_result_cb: %d\r\n", b);
+}
+bool ICACHE_FLASH_ATTR _json_deal_cb(void *arg, Wifi_Comm_type_t type, cJSON * pJsonRoot, void *p)
+{
+
+    cJSON *p_ota1 = cJSON_GetObjectItem(pJsonRoot, "ota");
+    if(p_ota1)
+    {
+        zlib_ota_start(p_ota1->valuestring);
+    }
+
+    char *s = cJSON_Print(pJsonRoot);
+    os_printf("pJsonRoot: \r\n%s\r\n", s);
+
+    switch (type)
+    {
+        case WIFI_COMM_TYPE_UDP:
+            zlib_udp_reply(arg, s);
+            break;
+        case WIFI_COMM_TYPE_TCP:
+            zlib_tcp_reply(arg, s);
+            break;
+        case WIFI_COMM_TYPE_HTTP:
+            zlib_web_server_reply(arg, true, APPLICATIOIN_JSON, s);
+            break;
+    }
+    cJSON_free((void *) s);
+
+}
 
 user_config_t user_config;
-
-
+static os_timer_t _timer_main;
+static void _user_timer_fun(void *arg)
+{
+    static uint32_t size_last;
+    uint32_t size;
+    size = system_get_free_heap_size();
+    if(size_last != size)
+    {
+        size_last = size;
+        os_printf("system_get_free_heap_size:%d\n", size);
+    }
+}
 void user_init(void)
 {
-	uart_init(115200, 115200);
+    uint8_t i;
+    uart_init(115200, 115200);
     //os_delay_us(60000);
-	os_printf(" \n \nStart user%d.bin\n", system_upgrade_userbin_check() + 1);
-	os_printf("SDK version:%s\n", system_get_sdk_version());
-	os_printf("FW version:%s\n", VERSION);
-	//UART_SetPrintPort(1);
-	os_printf(" \n \nStart user%d.bin\n", system_upgrade_userbin_check() + 1);
-	os_printf("SDK version:%s\n", system_get_sdk_version());
-	os_printf("FW version:%s\n", VERSION);
+    os_printf(" \n \nStart user%d.bin\n", system_upgrade_userbin_check() + 1);
+    os_printf("SDK version:%s\n", system_get_sdk_version());
+    os_printf("FW version:%s\n", VERSION);
+    //UART_SetPrintPort(1);
+    os_printf(" \n \nStart user%d.bin\n", system_upgrade_userbin_check() + 1);
+    os_printf("SDK version:%s\n", system_get_sdk_version());
+    os_printf("FW version:%s\n", VERSION);
 
-	zlib_wifi_init(false);
+    zlib_wifi_init(false);
+
+    zlib_web_wifi_init();
+    //zlib_mqtt_init(1);
 //	user_io_init();
 //	user_setting_init();
 //	user_wifi_init();
 //	user_sntp_init();
 //	user_os_timer_init();
-	//UDP³õÊ¼»¯,¼àÌı¶Ë¿Ú10182,µ±½ÓÊÕµ½ÌØ¶¨×Ö·û´®Ê±,·µ»Ø±¾Éè±¸IP¼°MACµØÖ·
+    //UDPåˆå§‹åŒ–,ç›‘å¬ç«¯å£10182,å½“æ¥æ”¶åˆ°ç‰¹å®šå­—ç¬¦ä¸²æ—¶,è¿”å›æœ¬è®¾å¤‡IPåŠMACåœ°å€
 //	user_devicefind_init(10182);
 //	user_webserver_init(80);
-//	TCP³õÊ¼»¯,¼àÌı¶Ë¿Ú10191
+//	TCPåˆå§‹åŒ–,ç›‘å¬ç«¯å£10191
 //	user_tcp_init(10191);
-	//TCP³õÊ¼»¯ 80¶Ë¿Ú webserver
-	//user_webserver_init(80);
+    //TCPåˆå§‹åŒ– 80ç«¯å£ webserver
+    //user_webserver_init(80);
+
+    zlib_udp_init(10181);
+    zlib_tcp_init(10182);
+    zlib_json_init(_json_deal_cb);
+    zlib_ota_set_result_callback(_ota_result_cb);
+
+    os_timer_disarm(&_timer_main);
+    os_timer_setfn(&_timer_main, (os_timer_func_t *) _user_timer_fun, NULL);
+    os_timer_arm(&_timer_main, 1000, true);
 
 }
